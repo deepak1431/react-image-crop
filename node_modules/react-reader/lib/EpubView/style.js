fdc56b7e4bf0fceaf03ab1fe4a0fddca97b864d9
mqfullstack-2 <deepak.multiqos@gmail.com>
2022-08-01 15:25:40 +0530

    react-image-crop

---
+++ node_modules/react-reader/lib/EpubView/style.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var epubViewStyles = {
+  viewHolder: {
+    position: 'relative',
+    height: '100%',
+    width: '100%'
+  },
+  view: {
+    height: '100%'
+  }
+};
+var _default = epubViewStyles;
+exports.default = _default;
